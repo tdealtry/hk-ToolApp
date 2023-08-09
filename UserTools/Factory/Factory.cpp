5f388b966ce352f6c43d058428431afb4b0063f5
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2023-08-09 18:57:03 +0100

    Save the ghost tools so far in this branch

--- UserTools/Factory/Factory.cpp
+++ UserTools/Factory/Factory.cpp
@@ -1,11 +1,9 @@
 #include "Factory.h"
 
 Tool* Factory(std::string tool) {
-	Tool* ret = 0;
+Tool* ret=0;
 
-	// if (tool=="Type") tool=new Type;
-	if(tool == "DummyTool")
-		ret = new DummyTool;
-
-	return ret;
+// if (tool=="Type") tool=new Type;
+if (tool=="WCSim_exe") ret=new HK::GHOST::WCSim_exe;
+return ret;
 }
--- UserTools/Unity.h
+++ UserTools/Unity.h
@@ -1,2 +1,1 @@
-///< Unity header to provide a clean Tool header list for factory and scripts ot add new Tools to
-#include "DummyTool.h"
+#include <WCSim_exe.h>
