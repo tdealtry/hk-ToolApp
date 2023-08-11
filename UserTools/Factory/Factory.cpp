3a2d9fe2285e7517bc12a229f9e0edca7719b907
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2023-08-11 12:30:02 +0100

    Add HKG4DetectorConstruction and HKG4TrackingAction tools

--- UserTools/Factory/Factory.cpp
+++ UserTools/Factory/Factory.cpp
@@ -4,6 +4,8 @@ Tool* Factory(std::string tool) {
 Tool* ret=0;
 
 // if (tool=="Type") tool=new Type;
-if (tool=="WCSim_exe") ret=new HK::GHOST::WCSim_exe;
+ if (tool=="HKG4TrackingAction") ret=new HK::GHOST::G4::HKG4TrackingAction;
+ if (tool=="WCSim_exe") ret=new HK::GHOST::G4::WCSim_exe;
+ if (tool=="HKG4DetectorConstruction") ret=new HK::GHOST::G4::HKG4DetectorConstruction;
 return ret;
 }
--- UserTools/Unity.h
+++ UserTools/Unity.h
@@ -1,1 +1,3 @@
+#include <HKG4TrackingAction.h>
 #include <WCSim_exe.h>
+#include <HKG4DetectorConstruction.h>
