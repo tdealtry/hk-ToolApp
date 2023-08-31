9387b74df3c39aebc95cfd14f5593bdb43cc0f52
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2023-08-31 17:06:06 +0100

    Add new GHOST tools

--- UserTools/Unity.h
+++ UserTools/Unity.h
@@ -1,3 +1,12 @@
+#include <HKG4DetectorConstruction.h>
 #include <HKG4TrackingAction.h>
 #include <WCSim_exe.h>
 #include <HKG4DetectorConstruction.h>
+#include <HKG4EventAction.h>
+#include <HKG4PhysicsListFactory.h>
+#include <HKG4PrimaryGeneratorAction.h>
+#include <HKG4RunAction.h>
+#include <HKG4StackingAction.h>
+#include <HKG4SteppingAction.h>
+#include <HKG4TrackingAction.h>
+#include <WCSim_exe.h>
