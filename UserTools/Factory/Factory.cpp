09bbb7f8b3f83c1ec2d0ce8b3129544895449eee
Clang Robot <robot@example.com>
2023-10-12 09:09:35 +0000

    Committing clang-format changes

--- UserTools/Factory/Factory.cpp
+++ UserTools/Factory/Factory.cpp
@@ -1,9 +1,10 @@
 #include "Factory.h"
 
 Tool* Factory(std::string tool) {
-Tool* ret=0;
+	Tool* ret = 0;
 
-// if (tool=="Type") tool=new Type;
-if (tool=="DummyTool") ret=new DummyTool;
-return ret;
+	// if (tool=="Type") tool=new Type;
+	if(tool == "DummyTool")
+		ret = new DummyTool;
+	return ret;
 }
